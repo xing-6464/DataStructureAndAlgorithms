f65392889dc58b5ed79ac72a27a2fba81e47a2e7
xing-6464 <17621404964@163.com>
2024-07-14 18:18:57 +0800

    向数组添加元素

--- src/dataStructure/Array.ts
+++ src/dataStructure/Array.ts
@@ -7,12 +7,34 @@ class MyArray<T> {
     this._size = 0
   }
 
-  get size(): number {
+  get length(): number {
     return this._size
   }
 
   get capacity(): number {
     return this._data.length
+  }
+
+  push(e: number) {
+    this.insert(this._size, e)
+  }
+
+  unshift(e: number) {
+    this.insert(0, e)
+  }
+
+  insert(index: number, e: number) {
+    if (this._size === this._data.length) {
+      throw new ReferenceError('add failed, array is full')
+    }
+    if (index < 0 || index > this._size) {
+      throw new RangeError('add failed, index out of range')
+    }
+    for (let i = this._size - 1; i >= index; i--) {
+      this._data[i + 1] = this._data[i]
+    }
+    this._data[index] = e
+    this._size++
   }
 
   isEmpty(): boolean {
