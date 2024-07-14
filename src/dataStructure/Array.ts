7bf89517f9f3f7ebce2e509df39b8df955006a96
xing-6464 <17621404964@163.com>
2024-07-14 19:42:27 +0800

    数组中查询搜索实现

--- src/dataStructure/Array.ts
+++ src/dataStructure/Array.ts
@@ -1,6 +1,7 @@
-class MyArray<T> {
+export default class MyArray<T> {
   private _data: number[]
-  private _size: number
+  private _size: number;
+  [index: number]: number
 
   constructor(capacity: number = 10) {
     this._data = new Array(capacity)
@@ -39,5 +40,33 @@ class MyArray<T> {
 
   isEmpty(): boolean {
     return this._size === 0
+  }
+
+  toString() {
+    let res = ''
+    res += `Array: length = ${this._size}, capacity = ${this._data.length}\n`
+    res += `[`
+    for (let i = 0; i < this._size; i++) {
+      res += `${this._data[i]}`
+      if (i !== this._size - 1) {
+        res += `, `
+      }
+    }
+    res += `]`
+    return res
+  }
+
+  get(index: number) {
+    if (index < 0 || index >= this._size) {
+      throw new RangeError('get failed, index out of range')
+    }
+    return this._data[index]
+  }
+
+  set(index: number, e: number) {
+    if (index < 0 || index >= this._size) {
+      throw new RangeError('set failed, index out of range')
+    }
+    this._data[index] = e
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-import { sortTest } from './test'
-import { generateRandomArray } from './utils'
+import MyArray from './dataStructure/Array'
 
-const dataSize = [10000, 100000]
-for (const size of dataSize) {
-  const arr = generateRandomArray(size)
-  sortTest('insertionSort', arr)
+const arr = new MyArray(20)
+for (let i = 0; i < 10; i++) {
+  arr.push(i)
 }
+
+console.log(arr.toString()) // output: 0,1,2,3,4,5,6,7,8,9
+arr.insert(1, 100)
+console.log(arr.toString())
+
+arr.unshift(-1)
+
+console.log(arr.toString())
+
+console.log(arr.get(1))
+console.log(arr[1])
