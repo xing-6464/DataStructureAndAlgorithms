de0a76e33dc04705bdaa56ec5f907a2de35d83f5
unknown <xingguang64@yeah.net>
2024-09-14 18:29:35 +0800

    二分查找算法的非递归实现

--- src/algorithms/search/BinarySearch.ts
+++ src/algorithms/search/BinarySearch.ts
@@ -1,15 +1,41 @@
 export class BinarySearch {
   private constructor() {}
 
+  // 递归
+  static searchR<T>(
+    data: T[],
+    target: T,
+    compare: <T>(a: T, target: T) => number = this.compare
+  ): number {
+    return this._searchR(data, target, compare, 0, data.length - 1)
+  }
+
+  // 非递归
   static search<T>(
     data: T[],
     target: T,
     compare: <T>(a: T, target: T) => number = this.compare
   ): number {
-    return this._search(data, target, compare, 0, data.length - 1)
+    let l = 0
+    let r = data.length - 1
+
+    while (l <= r) {
+      const i = l + Math.floor((r - l) / 2)
+
+      if (compare(data[i], target) === 0) {
+        return i
+      }
+      if (compare(data[i], target) < 0) {
+        l = i + 1
+      } else {
+        r = i - 1
+      }
+    }
+    return -1
   }
 
-  private static _search<T>(
+  // 递归
+  private static _searchR<T>(
     data: T[],
     target: T,
     compare: (a: T, b: T) => number,
@@ -25,10 +51,10 @@ export class BinarySearch {
     }
 
     if (compare(data[mid], target) < 0) {
-      return this._search(data, target, compare, mid + 1, r)
+      return this._searchR(data, target, compare, mid + 1, r)
     }
 
-    return this._search(data, target, compare, l, mid - 1)
+    return this._searchR(data, target, compare, l, mid - 1)
   }
 
   private static compare<T>(a: T, b: T): number {
