addaa5d266459f1aae97a0e2b382cc41fa7d976b
unknown <xingguang64@yeah.net>
2024-09-16 16:58:24 +0800

    改进添加操作：深入理解递归终止条件

--- src/dataStructure/Tree/BST.ts
+++ src/dataStructure/Tree/BST.ts
@@ -20,32 +20,36 @@ export class BST<T extends { valueOf(): number }> {
   }
 
   add(e: T) {
-    if (this.root === null) {
-      this.root = new Node(e)
-      this.size++
-    } else {
-      this._add(this.root, e)
-    }
+    this.root = this._add(this.root, e)
   }
 
-  private _add(node: Node<T>, e: T) {
-    if (e.valueOf() === node.e.valueOf()) {
-      return
-    } else if (e.valueOf() < node.e.valueOf() && node.left === null) {
-      node.left = new Node(e)
+  // 向node为根的二叉搜索树中插入元素e，递归算法
+  // 返回插入新节点后二分搜索树的根
+  private _add(node: Node<T> | null, e: T) {
+    // if (e.valueOf() === node.e.valueOf()) {
+    //   return
+    // } else if (e.valueOf() < node.e.valueOf() && node.left === null) {
+    //   node.left = new Node(e)
+    //   this.size++
+    //   return
+    // } else if (e.valueOf() > node.e.valueOf() && node.right === null) {
+    //   node.right = new Node(e)
+    //   this.size++
+    //   return
+    // }
+
+    if (node === null) {
       this.size++
-      return
-    } else if (e.valueOf() > node.e.valueOf() && node.right === null) {
-      node.right = new Node(e)
-      this.size++
-      return
+      return new Node(e)
     }
 
     if (e.valueOf() < node.e.valueOf()) {
-      this._add(node.left!, e)
-    } else {
-      this._add(node.right!, e)
+      node.left = this._add(node.left!, e)
+    } else if (e.valueOf() > node.e.valueOf()) {
+      node.right = this._add(node.right!, e)
     }
+
+    return node
   }
 
   isEmpty(): boolean {
