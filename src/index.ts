8d6795f7df6389bf1b17222a55b9acd3f407c310
unknown <xingguang64@yeah.net>
2024-10-05 08:38:16 +0800

    希尔排序算法小优化

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { sortTest } from './test'
 import { ArrayUtils } from './utils/index'
 
-let n = 1000000
+let n = 10000000
 const arr = ArrayUtils.generateRandomArray(n)
 const arr2 = arr.slice()
 const arr3 = arr.slice()
